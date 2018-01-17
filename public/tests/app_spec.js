02efd864ea3c2e7c968cc3c765554ecb694e3605
murayama <murayama@murayama-no-MacBook-Pro.local>
2018-01-17 10:28:24 +0900

    update chap3

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -6,7 +6,7 @@ describe('LearnJS', function() {
 
   it('show the landing page view when there is no hash', function(){
     learnjs.showView('')
-    expect($('.view-container .landing-page').length).toEqual(1)
+    expect($('.view-container .landing-view').length).toEqual(1)
   });
 
   it('passes the hash view parameter to the view function', function(){
@@ -18,8 +18,18 @@ describe('LearnJS', function() {
   describe('problem view', function(){
     it('has a title that includes the problem number', function(){
       var view = learnjs.problemView('1')
-      expect(view.text()).toEqual('Problem #1 Coming soon!');
+      expect(view.find('.title').text()).toEqual('Problem #1');
     });
+    it('show the problem code', function(){
+      var view = learnjs.problemView('1')
+      expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+    });
+
+    it('show the description', function(){
+      var view = learnjs.problemView('1')
+      expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+    });
+
   });
 
   it('invokes the router than loaded', function(){
@@ -34,4 +44,20 @@ describe('LearnJS', function() {
     $(window).trigger('hashchange')
     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
   });
+
+  describe('answer section', function(){
+    it('can check a correct answer by hitting a button', function(){
+        var view = learnjs.problemView('1')
+        view.find('.answer').val('true');
+        view.find('.check-btn').click();
+        expect(view.find('.result').text()).toContain('Correct!');
+    });
+    it('rejects an incorrect answer', function(){
+      var view = learnjs.problemView('1')
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
+    });
+
+  })
 });
